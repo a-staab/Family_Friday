cac6b2612542b80d268f3bb163d7f0c64d61a7ad
a-staab <astaab@gmail.com>
2018-01-20 21:50:39 +0000

    Added form for adding new employees

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, render_template
 from model import db, connect_to_db
 from flask_debugtoolbar import DebugToolbarExtension
+import os
 
 app = Flask(__name__)
-
+app.secret_key = os.environ["SECRET_KEY"]
 
 @app.route("/")
 def show_main():
@@ -16,7 +17,7 @@ def show_main():
 connect_to_db(app)
 
 if __name__ == "__main__":
-    app.debug = False
+    app.debug = True
     app.jinja_env.auto_reload = app.debug
     connect_to_db(app)
     DebugToolbarExtension(app)
