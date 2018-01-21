3be7ca217c92b4690731030bc0ece9cd3be46d91
a-staab <astaab@gmail.com>
2018-01-21 05:11:25 +0000

    Wrote function to distribute number of teammates among tables of 3, 4 and/or 5.

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template
 from model import db, connect_to_db
 from flask_debugtoolbar import DebugToolbarExtension
+from random import choice
+from model import Employee
 import os
 
 app = Flask(__name__)
@@ -10,8 +12,48 @@ app.secret_key = os.environ["SECRET_KEY"]
 def show_main():
     """Return landing page."""
 
+
+
+
     return render_template("main.html")
 
+
+def get_table_sizes(teammates):
+    """Given a list of teammates, returns a list of tuples of type
+    (teammates per table, number of tables) such that the sum of the
+    products of the number of tables and teammates per table for all of the
+    tuples is equal to the total number of teammates."""
+
+    if len(teammates) % 5 == 0:
+        return [(5, (teammates / 5))]
+
+    # Checking if the teammates are evenly divisible by 4 and 3 helps
+    # ensure some variety in the size of groups (assuming they're growing
+    # relatively steadily). Otherwise, most people would dine in a group of
+    # five most of the time.
+    elif len(teammates) % 4 == 0:
+        return [(4, (teammates / 4))]
+
+    elif len(teammates) % 3 == 0:
+        return [(3, (teammates / 3))]
+
+    else:
+        remainder = len(teammates) % 5
+        if remainder == 1:
+            return [(5, (teammates / 5 - 1), (3, 2))]
+        elif remainder == 2:
+            return [(5, (teammates / 5 - 1), (4, 1), (3, 1))]
+        elif remainder == 3:
+            return [(5, teammates / 5), (3, 1)]
+        elif remainder == 4:
+            return [(5, teammates / 5), (4, 1)]
+
+    # TODO: Possibly write recursively?
+
+
+def groupify():
+
+    teammates = Employee.query.all()
 
 
 connect_to_db(app)
