99f021351888bc0000d918920b111bcfaec9713f
a-staab <astaab@gmail.com>
2018-01-22 16:55:56 +0000

    Added sample data to model for testing

--- model.py
+++ model.py
@@ -25,6 +25,28 @@ def connect_to_db(app, db_uri='postgresql:///staff_directory'):
     db.init_app(app)
 
 
+def example_data():
+    """Sample data for testing."""
+
+    # Tests using instantiation
+    jack = Employee(first_name="Jack", last_name="Toler")
+    paul = Employee(first_name="Paul", last_name="Johnson")
+    anna = Employee(first_name="Anna", last_name="Whitley")
+    ethan = Employee(first_name="Ethan", last_name="Pratt")
+    latoya = Employee(first_name="Latoya", last_name="Erickson")
+    janalynn = Employee(first_name="Janalynn", last_name="Headly")
+    scott = Employee(first_name="Scott", last_name="Davis")
+    jamal = Employee(first_name="Jamal", last_name="Curwood")
+    evelyn = Employee(first_name="Evelyn", last_name="Howe")
+    mariah = Employee(first_name="Mariah", last_name="Dawson")
+    jamie = Employee(first_name="Jamie", last_name="Finch")
+
+    db.session.add_all([jack, paul, anna, ethan, latoya, janalynn, scott, jamal,
+                        evelyn, mariah, jamie])
+
+    db.session.commit()
+
+
 if __name__ == "__main__":
     from server import app
     connect_to_db(app)
